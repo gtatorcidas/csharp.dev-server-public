adc812da51f58f4bb5590424e1d82fb0d8eb1db6
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2023-11-05 18:45:00 -0300

    feat: add EntityFramework DbContext setup

---
+++ src/Torcidas/Torcidas.Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Torcidas.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+       {
+
+       }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
---
+++ src/Torcidas/Torcidas.Infra/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Torcidas.Infra.Data
+{
+    public class ApplicationDbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args) {
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
+
+            builder.UseNpgsql(configuration.GetConnectionString("Default"));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
