c0feb7aeec0d7c2ce92bc56fa263747197ec315c
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2023-11-05 18:30:37 -0300

    feat: add launch settings and Program.cs files

---
+++ src/Torcidas/Torcidas.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using SampSharp.Core;
+using SampSharp.Entities;
+
+namespace Torcidas.UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new GameModeBuilder()
+                .RedirectConsoleOutput()
+                .UseEcs<Startup>()
+                .Run();
+        }
+    }
+}
